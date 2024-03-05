197e668ee5cae7aec937b196c09c9ad7b8c3a4c7
sajiwaniDewi <dewisanjiw@gmail.com>
2024-03-05 13:32:45 +0900

    Feat: show Detail Leader data

--- src/pages/LeaderDetail.jsx
+++ src/pages/LeaderDetail.jsx
@@ -1,13 +1,36 @@
-import { useState } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
 const LeaderDetail = () => {
     const [leaderData, setLeaderData] = useState({});
+
+    const { id } = useParams();
+
+    const getLeaderDetail = () => {
+        axios
+            .get(`https://reqres.in/api/users/${id}`)
+            .then((res) => {
+                setLeaderData(res.data.data);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    };
+
+    useEffect(() => {
+        getLeaderDetail();
+    }, []);
+
     return (
         <>
             <h2>Detail Leader</h2>
             <div>
-                <img src="" alt="avatar" />
-                <h3>name</h3>
-                <p>email</p>
+                <img src={leaderData.avatar} alt="avatar" />
+                <h3>
+                    {leaderData.first_name} {leaderData.last_name}
+                </h3>
+                <p>{leaderData.email}</p>
             </div>
         </>
     );
