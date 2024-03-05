6dd7341771bb4172c6a85b6c515128e9c136476e
sajiwaniDewi <dewisanjiw@gmail.com>
2024-03-05 12:40:47 +0900

    feat: create reusable table component

---
+++ src/components/Table.jsx
@@ -0,0 +1,51 @@
+const Table = ({ leaderData }) => {
+    let header = [
+        ...new Set(
+            data
+                .map((item) => Object.keys(item))
+                .join(",")
+                .split(",")
+        ),
+    ].map((item) => (item === "id" ? "No" : item));
+
+    return (
+        <table>
+            <thead>
+                <tr>
+                    {header.map((item, index) => (
+                        <th key={index}>{item}</th>
+                    ))}
+                </tr>
+            </thead>
+            <tbody>
+                {data.map((item, indexs) => (
+                    <tr key={indexs}>
+                        {header.map((head, index) => {
+                            if (index === 1) {
+                                return (
+                                    <td key={index}>
+                                        <Link
+                                            to={`leader-detail/${item["id"]}`}
+                                        >
+                                            {item[head]}
+                                        </Link>{" "}
+                                    </td>
+                                );
+                            } else if (head === "No") {
+                                if (typeof item["id"] === "number") {
+                                    return <td key={index}>{item["id"]}</td>;
+                                } else {
+                                    return <td key={index}>{indexs + 1}</td>;
+                                }
+                            } else {
+                                return <td key={index}>{item[head]}</td>;
+                            }
+                        })}
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    );
+};
+
+export default Table;
