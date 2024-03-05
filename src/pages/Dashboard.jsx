dedd2e5f1be6caf7a0f3ff250885e8fa42f13084
sajiwaniDewi <dewisanjiw@gmail.com>
2024-03-05 12:41:59 +0900

    feat: Dasbord page and handle data for table

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import Table from "../components/Table";
+import axios from "axios";
+
+const Dashboard = () => {
+    const [leader, setLeader] = useState([]);
+    const [page, setPage] = useState({
+        page: 1,
+        per_page: 0,
+        total: 0,
+        total_pages: 0,
+    });
+
+    const handleLeaderData = () => {
+        axios.get(`https://reqres.in/api/users?page=${page}`).then((res) => {
+            const dataLead = [];
+            res.data.data.map((item) => {
+                const data = {
+                    id: item.id,
+                    name: `${item.first_name} ${item.last_name}`,
+                    email: item.email,
+                };
+                setPage({
+                    page: res.data.page,
+                    per_page: res.data.per_page,
+                    total: res.data.total,
+                    total_pages: res.data.total_pages,
+                });
+
+                dataLead.push(data);
+            });
+            setLeader(dataLead);
+        });
+    };
+
+    useEffect(() => {
+        handleDataLeader(page);
+    }, [page]);
+
+    return (
+        <>
+            <h1> Dashboard</h1>
+            <Table leaderData={leader} />
+        </>
+    );
+};
+
+export default Dashboard;
