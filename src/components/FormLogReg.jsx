9d9b6e917481136b8356411cf5a0b2ea4287a1cc
sajiwaniDewi <dewisanjiw@gmail.com>
2024-03-05 10:20:55 +0900

    feat: handle onchange email and password

--- src/components/FormLogReg.jsx
+++ src/components/FormLogReg.jsx
@@ -1,11 +1,38 @@
+import axios from "axios";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+
 const FormLogReg = ({ title }) => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [notif, setNotif] = useState("");
+    const [loading, setLoading] = useState(false);
+
+    const navigate = useNavigate();
+
+    const handleEmail = (e) => {
+        setEmail(e.target.value);
+    };
+
+    const handlePassword = (e) => {
+        setPassword(e.target.value);
+    };
+
     return (
         <div>
             <h1>{title}</h1>
 
             <div>
-                <input type="email" placeholder="email" />
-                <input type="password" placeholder="password" />
+                <input
+                    type="email"
+                    placeholder="email"
+                    onChange={handleEmail}
+                />
+                <input
+                    type="password"
+                    placeholder="password"
+                    onChange={handlePassword}
+                />
             </div>
             <button type="submit">Submit</button>
         </div>
