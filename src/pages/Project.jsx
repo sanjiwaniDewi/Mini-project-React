cc429ecf5787d40a80108baa4c7ffd16d32e54f6
sajiwaniDewi <dewisanjiw@gmail.com>
2024-03-06 20:55:57 +0900

    Feat: Project page

---
+++ src/pages/Project.jsx
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom";
+import Layout from "../components/Layout";
+import Table from "../components/Table";
+import projects from "../data/project";
+
+const Project = () => {
+    return (
+        <Layout>
+            <h2>Project page</h2>
+            <Table data={projects} />
+            <Link to="/new-project">Create New Project</Link>
+        </Layout>
+    );
+};
+
+export default Project;
