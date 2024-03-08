76afe00c767bde9580bb34558232ebaa43ff171c
sajiwaniDewi <dewisanjiw@gmail.com>
2024-03-08 13:15:22 +0900

    feat: handle onChange and submit new project form

--- src/pages/NewProject.jsx
+++ src/pages/NewProject.jsx
@@ -1,12 +1,81 @@
+import { useState } from "react";
 import Layout from "../components/Layout";
 
+import projects from "../data/project.js";
+
 const NewProject = () => {
+    const [newproject, setNewProject] = useState({
+        id: "",
+        name: "",
+        team: "",
+        start: "",
+        end: "",
+        detail: "",
+    });
+
+    let uuid = self.crypto.randomUUID();
+    let start = new Date().toLocaleDateString();
+
+    const handleEndDateFormat = (date) => {
+        return date.split("-").reverse().join("-");
+    };
+    const handleStartDateFormat = (date) => {
+        const start = date.split("/");
+        start[0].length < 2 ? (start[0] = 0 + start[0]) : start[0];
+
+        return start.join("-");
+    };
+
+    const handleChange = (e) => {
+        if (e.target.name === "end") {
+            setNewProject({
+                ...newproject,
+                id: uuid,
+                start: handleStartDateFormat(start),
+                [e.target.name]: handleEndDateFormat(e.target.value),
+            });
+        } else {
+            setNewProject({
+                ...newproject,
+                id: uuid,
+                start: handleStartDateFormat(start),
+                [e.target.name]: e.target.value,
+            });
+        }
+    };
+
+    const handleSubmit = (e) => {
+        console.log(newproject);
+
+        projects.push(newproject);
+
+        setNewProject({
+            id: "",
+            name: "",
+            team: "",
+            start: "",
+            end: "",
+            detail: "",
+        });
+    };
+
     return (
         <Layout>
             <h2>New Project</h2>
             <div style={{ display: "flex", flexDirection: "column", gap: 5 }}>
-                <input type="text" placeholder="project name" />
-                <select name="team" id="team">
+                <input
+                    type="text"
+                    name="name"
+                    placeholder="project name"
+                    value={newproject.name}
+                    onChange={handleChange}
+                />
+                <select
+                    name="team"
+                    id="team"
+                    value={newproject.team}
+                    onChange={handleChange}
+                >
                     <option>Team</option>
                     <option value="Ayam">Ayam</option>
                     <option value="Kambing">Kambing</option>
@@ -14,7 +83,12 @@ const NewProject = () => {
                     <option value="Sapi">Sapi</option>
                 </select>
 
-                <input type="date" />
+                <input
+                    name="end"
+                    type="date"
+                    value={newproject.date}
+                    onChange={handleChange}
+                />
 
                 <textarea
                     name="detail"
@@ -22,10 +96,14 @@ const NewProject = () => {
                     cols="30"
                     rows="10"
                     placeholder="Description"
+                    value={newproject.detail}
+                    onChange={handleChange}
                 ></textarea>
 
                 <input type="file" />
-                <button type="submit">Submit</button>
+                <button type="submit" onClick={handleSubmit}>
+                    Submit
+                </button>
             </div>
         </Layout>
     );
