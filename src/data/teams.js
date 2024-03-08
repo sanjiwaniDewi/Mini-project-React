17e7db2094705d9f116d4aea481d7f896c3a0677
sajiwaniDewi <dewisanjiw@gmail.com>
2024-03-08 14:52:29 +0900

    Feat:new data teams

---
+++ src/data/teams.js
@@ -0,0 +1,25 @@
+const teams = [
+    {
+        id: 1,
+        name: "mawar",
+        lead: "Antonio",
+        members: [
+            {
+                name: "alex",
+                job: "UX UI designer",
+            },
+
+            {
+                name: "david",
+                job: "frontend",
+            },
+            {
+                name: "alex",
+                job: "backend",
+            },
+        ],
+        createdAt: "08-02-2024",
+    },
+];
+
+export default teams;
