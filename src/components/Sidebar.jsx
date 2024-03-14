2a30055cccfe108bd965ba43cb315f7a1618ccaa
sajiwaniDewi <dewisanjiw@gmail.com>
2024-03-14 16:03:07 +0900

    style: resturctured position element for styling sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,33 +1,54 @@
 import { Link, useNavigate } from "react-router-dom";
+import { RiDashboardLine } from "react-icons/ri";
+import { GrTasks } from "react-icons/gr";
+import { RiTeamLine } from "react-icons/ri";
+import { MdLogout } from "react-icons/md";
+import { GrClose } from "react-icons/gr";
 
-const Sidebar = ({ handle }) => {
-    const navigate = useNavigate();
-    const handleLogout = () => {
-        localStorage.removeItem("access_token");
-        setTimeout(() => {
-            navigate("/login");
-        }, 1000);
-    };
+import "../style/components.css";
 
+const Sidebar = ({ handleShow, handleLogout }) => {
     return (
-        <div className="sidebar">
+        <div className="sidebar ">
             <div className="inside-sidebar">
-                <div className="side-button">
-                    <button onClick={handle}>Close</button>
-                </div>
-                <h2>Sidebar</h2>
-                <ul>
-                    <li>
-                        <Link to="/">Dashboard</Link>
+                <button
+                    className="sidebar-button sidebar-btn-close"
+                    onClick={handleShow}
+                >
+                    <GrClose />
+                    Close
+                </button>
+
+                <h2>PRO.CO</h2>
+                <ul className="sidebar-nav">
+                    <li className="sidebar-menu">
+                        <Link to="/">
+                            {" "}
+                            <RiDashboardLine />
+                            Dashboard
+                        </Link>
                     </li>
-                    <li>
-                        <Link to="/project">Project</Link>
+                    <li className="sidebar-menu">
+                        <Link to="/project">
+                            {" "}
+                            <GrTasks />
+                            Project
+                        </Link>
                     </li>
-                    <li>
-                        <Link to="/team">Team</Link>
+                    <li className="sidebar-menu">
+                        <Link to="/team">
+                            <RiTeamLine />
+                            Team
+                        </Link>
                     </li>
-                    <li>
-                        <button onClick={handleLogout}>Logout</button>
+                    <li className="sidebar-menu">
+                        <button
+                            className="sidebar-button"
+                            onClick={handleLogout}
+                        >
+                            <MdLogout />
+                            Logout
+                        </button>
                     </li>
                 </ul>
             </div>
