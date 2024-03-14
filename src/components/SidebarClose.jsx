a3ec61d9132fe40859552b2b6ac5ab300ea4576d
sajiwaniDewi <dewisanjiw@gmail.com>
2024-03-14 16:06:20 +0900

    Feat: new Sidebar close

---
+++ src/components/SidebarClose.jsx
@@ -0,0 +1,49 @@
+import { RiDashboardLine } from "react-icons/ri";
+import { GrTasks } from "react-icons/gr";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { RiTeamLine } from "react-icons/ri";
+import { MdLogout } from "react-icons/md";
+import { Link } from "react-router-dom";
+
+const SidebarClose = ({ handleShow, handleLogout }) => {
+    return (
+        <div className="sidebar sidebar-close">
+            <div className="inside-sidebar-close">
+                <button
+                    onClick={handleShow}
+                    className="sidebar-button sidebar-btn-open"
+                >
+                    <GiHamburgerMenu />{" "}
+                </button>
+                <h2>PC</h2>
+                <ul className="sidebar-nav sidebar-nav-close">
+                    <li className="sidebar-menu sidebar-menu-close">
+                        <Link to="/">
+                            <RiDashboardLine />
+                        </Link>
+                    </li>
+                    <li className="sidebar-menu sidebar-menu-close">
+                        <Link to="/project">
+                            <GrTasks />
+                        </Link>
+                    </li>
+                    <li className="sidebar-menu sidebar-menu-close">
+                        <Link to="/team">
+                            <RiTeamLine />
+                        </Link>
+                    </li>
+                    <li className="sidebar-menu sidebar-menu-close">
+                        <button
+                            className="sidebar-button"
+                            onClick={handleLogout}
+                        >
+                            <MdLogout alt="Logout" />
+                        </button>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export default SidebarClose;
