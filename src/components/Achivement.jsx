a1d972c24ec4e69e5471cb6a605186f8e37534b4
sajiwaniDewi <dewisanjiw@gmail.com>
2024-03-15 08:58:40 +0900

    Feat: add achivment and Experiences in detail leader page

---
+++ src/components/Achivement.jsx
@@ -0,0 +1,25 @@
+import "../style/components.css";
+
+const Achivement = () => {
+    return (
+        <div className="leader-achivements">
+            <div className="finish">
+                <h5>Finish</h5>
+                <h2>105</h2>
+                <p>Projects</p>
+            </div>
+            <div className="ongoing">
+                <h5>On Going</h5>
+                <h2>15</h2>
+                <p>Projects</p>
+            </div>
+            <div className="team">
+                <h5>Join</h5>
+                <h2>11</h2>
+                <p>Teams</p>
+            </div>
+        </div>
+    );
+};
+
+export default Achivement;
---
+++ src/components/ExperiencesAndSkils.jsx
@@ -0,0 +1,30 @@
+import "../style/components.css";
+
+const ExperiencesAndSkils = () => {
+    return (
+        <div className="leader-background">
+            <p>
+                <strong>Experiences:</strong> Lorem ipsum, dolor sit amet
+                consectetur adipisicing elit. Ad quidem natus modi, sunt ipsa
+                nisi dicta dolores quae fugiat eligendi? Hic unde et ullam?
+                Doloribus quas nobis impedit iusto totam.
+            </p>
+            <div className="leader-skils">
+                <p>
+                    <strong>Skils:</strong>
+                </p>
+                <ul>
+                    <li>Html</li>
+                    <li>Node</li>
+                    <li>Golang</li>
+                    <li>JavaScript</li>
+                    <li>Python</li>
+                    <li>MySQL</li>
+                    <li>CSS</li>
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export default ExperiencesAndSkils;
