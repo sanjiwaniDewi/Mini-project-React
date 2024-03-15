d424913dda7b036dc93fc7fb6e8ba946114c4f68
sajiwaniDewi <dewisanjiw@gmail.com>
2024-03-16 05:45:38 +0900

    Feat: centralize api url into environment file

---
+++ src/environments/constant.jsx
@@ -0,0 +1,7 @@
+import { environment } from "./environment";
+
+export const constant = {
+    login: `${environment.API_URL}/login`,
+    register: `${environment.API_URL}/register`,
+    user: `${environment.API_URL}/users`,
+};
