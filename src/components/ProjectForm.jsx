33c54e1b07fd56c8cfe57ec4e704ebf28ca130a3
sajiwaniDewi <dewisanjiw@gmail.com>
2024-03-16 06:44:35 +0900

    Refactor: team and project form page to component form

---
+++ src/components/ProjectForm.jsx
@@ -0,0 +1,116 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import projects from "../data/project.js";
+import teams from "../data/teams.js";
+
+import "../style/components.css";
+
+const ProjectForm = () => {
+    const [newproject, setNewProject] = useState({
+        id: "",
+        project: "",
+        team: "",
+        start: "",
+        end: "",
+        detail: "",
+    });
+
+    const navigate = useNavigate();
+
+    let uuid = self.crypto.randomUUID();
+    let start = new Date().toLocaleDateString();
+
+    const handleEndDateFormat = (date) => {
+        return date.split("-").reverse().join("-");
+    };
+    const handleStartDateFormat = (date) => {
+        const start = date.split("/");
+        start[0].length < 2 ? (start[0] = 0 + start[0]) : start[0];
+
+        return start.join("-");
+    };
+
+    const handleChange = (e) => {
+        if (e.target.name === "end") {
+            setNewProject({
+                ...newproject,
+                id: uuid,
+                start: handleStartDateFormat(start),
+                [e.target.name]: handleEndDateFormat(e.target.value),
+            });
+        } else {
+            setNewProject({
+                ...newproject,
+                id: uuid,
+                start: handleStartDateFormat(start),
+                [e.target.name]: e.target.value,
+            });
+        }
+    };
+
+    const handleSubmit = (e) => {
+        console.log(newproject);
+
+        projects.push(newproject);
+
+        setNewProject({
+            id: "",
+            project: "",
+            team: "",
+            start: "",
+            end: "",
+            detail: "",
+        });
+        setTimeout(() => {
+            navigate("/project");
+        }, 1500);
+    };
+
+    return (
+        <div className="form">
+            <input
+                type="text"
+                name="project"
+                placeholder="project name"
+                value={newproject.project}
+                onChange={handleChange}
+            />
+            <select
+                name="team"
+                id="team"
+                value={newproject.team}
+                onChange={handleChange}
+            >
+                <option>Team</option>
+                {teams.length &&
+                    teams.map((person, index) => (
+                        <option key={index}>{person.team}</option>
+                    ))}
+            </select>
+
+            <input
+                name="end"
+                type="date"
+                value={newproject.date}
+                onChange={handleChange}
+            />
+
+            <textarea
+                name="detail"
+                id="detail"
+                cols="30"
+                rows="10"
+                placeholder="Description"
+                value={newproject.detail}
+                onChange={handleChange}
+            ></textarea>
+
+            <input type="file" />
+            <button type="submit" onClick={handleSubmit}>
+                Submit
+            </button>
+        </div>
+    );
+};
+
+export default ProjectForm;
