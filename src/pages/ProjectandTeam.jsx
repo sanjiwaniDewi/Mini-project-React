08884945ada021936d8819b91684b2c090a5791a
sajiwaniDewi <dewisanjiw@gmail.com>
2024-03-15 22:47:36 +0900

    Style: change  class name for styling

--- src/pages/ProjectandTeam.jsx
+++ src/pages/ProjectandTeam.jsx
@@ -3,11 +3,20 @@ import Layout from "../components/Layout";
 import Table from "../components/Table";
 import projects from "../data/project";
 
+import "../style/pages.css";
+
 const ProjectandTeam = ({ title, data, route }) => {
     return (
         <Layout title={`${title} Page`}>
-            <Table data={data} />
-            <Link to={route}>Create New {title}</Link>
+            <div className="content-main">
+                <Table data={data} />
+
+                <div className="button-create">
+                    <Link to={route}>
+                        <button>Create New {title}</button>
+                    </Link>
+                </div>
+            </div>
         </Layout>
     );
 };
