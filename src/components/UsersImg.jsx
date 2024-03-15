4f692baa8cd518dd489de1d191f7c66eb8af867a
sajiwaniDewi <dewisanjiw@gmail.com>
2024-03-15 13:04:22 +0900

    Feat: show avatar user  login

---
+++ src/components/UsersImg.jsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+import users from "../data/users";
+import axios from "axios";
+
+const UserImg = () => {
+    const [email, setEmail] = useState(localStorage.getItem("email"));
+
+    const [user, setUser] = useState("");
+    const [totalData, setTotalData] = useState("");
+
+    const handleTotalData = () => {
+        // setUser(users.filter((item) => item.email === email));
+        axios
+
+            .get("https://reqres.in/api/users")
+            .then((res) => setTotalData(res.data.total))
+            .catch((err) => console.log(err));
+    };
+
+    console.log(totalData);
+
+    useEffect(() => {
+        handleTotalData();
+    }, []);
+
+    const handleUser = () => {
+        axios
+            .get(`https://reqres.in/api/users?per_page=${totalData}`)
+            .then((res) =>
+                setUser(...res.data.data.filter((item) => item.email === email))
+            )
+            .catch((err) => console.log(err));
+    };
+
+    useEffect(() => {
+        handleUser();
+    }, [totalData]);
+    console.log(user);
+
+    return (
+        <div className="user">
+            <img className="user" src={user?.avatar} alt="user" />
+        </div>
+    );
+};
+
+export default UserImg;
